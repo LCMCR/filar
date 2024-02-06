ce4b60ead0acb7ad42023e9ddd9203688087acd1
Laura Collazo <laucollazo@hotmail.com>
2024-02-06 14:26:34 -0300

    fix: independent checkout page

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -1,22 +1,35 @@
 import PaypalButton from "../components/cart/PayPalButton";
 import PropTypes from 'prop-types';
 import { useSelector } from "react-redux";
+import { HiOutlineArrowLeft } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
 
 const Checkout = () => {
     const productData = useSelector((state) => state.filar.productData);
     const productDescription = productData.map(item => `${item.title} $ ${item.price} ${item.quantity}`).join(', ');
     const totalAmt = productData.reduce((total, item) => total + (item.price * item.quantity), 0).toFixed(2)+"";
-    console.log(productDescription);
-    console.log(totalAmt);
+    // console.log(productDescription);
+    // console.log(totalAmt);
+    const navigate = useNavigate();
+
+    const handleOnClick = () => {
+        navigate('/');
+    };
 
     return (
-        <div className='flex justify-center items-center h-[80vh]'>
+        <div className='w-screen flex justify-center items-center h-auto min-h-[80vh]'>
             <div className='max-w-md w-full p-6'>
                 <div className='bg-white rounded-lg shadow-lg p-8 text-center'>
-                    <h1 className='text-2xl font-titleFont text-stone-600 font-bold mb-4'>Checkout</h1>
+                    <h2 className='text-2xl font-titleFont text-stone-600 font-bold mb-4'>Checkout</h2>
                     <div>
                         <PaypalButton description={productDescription} total={totalAmt}/>
                     </div>
+                    <button onClick={handleOnClick} className="flex flex-row items-center gap-2 text-stone-600">
+                        <span>
+                            <HiOutlineArrowLeft />
+                        </span>
+                        back to Filar
+                    </button>
                 </div>
             </div>
         </div>
