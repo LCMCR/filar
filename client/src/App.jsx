41efc768fd01668ce2bf95d53e4238d9cecb7d45
Laura Collazo <laucollazo@hotmail.com>
2024-02-06 16:28:25 -0300

    feat: subscribe functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,102 +1,3 @@
-// import './index.css'
-// import Home from './pages/Home'
-// import AnnouncementBar from './components/static/AnnouncementBar';
-// import Header from './components/static/Header';
-// import Footer from './components/static/Footer';
-// import Cart from './pages/Cart';
-// import Shop from './pages/Shop';
-// import Product from './components/products/Product';
-// import NewIn from './pages/NewIn';
-// import Checkout from './pages/Checkout';
-// import AboutUs from './pages/AboutUs';
-// import Contact from './pages/Contact';
-// import {
-//   createBrowserRouter,
-//   Outlet,
-//   RouterProvider,
-//   ScrollRestoration,
-// } from "react-router-dom";
-// import { productsData } from './api/Api';
-// import Login from './pages/Login';
-// import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-
-// const initialOptions = {
-//   "client-id": import.meta.env.VITE_PAYPAL_CLIENT_ID,
-//   currency: "USD",
-//   intent: "capture",
-// };
-
-// const Layout = () => {
-//   return (
-//     <div>
-//       <AnnouncementBar title="Sail into our store!"/>
-//       <Header />
-//       <ScrollRestoration />
-//       <Outlet />
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children:[
-//       {
-//         path: "/",
-//         element: <Home />,
-//       },
-//       {
-//         path: "/shop",
-//         element: <Shop />,
-//         loader: productsData,
-//       },
-//       {
-//         path: "/product/:id",
-//         element: <Product />,
-//       },
-//       {
-//         path: "/cart",
-//         element: <Cart />,
-//       },
-//       {
-//         path: "/checkout",
-//         element: <Checkout />,
-//       },
-//       {
-//         path: "/contact",
-//         element: <Contact />,
-//       },
-//       {
-//         path: "/login",
-//         element: <Login />,
-//       },
-//       {
-//         path: "/about-us",
-//         element: <AboutUs />,
-//       },
-//       {
-//         path: "/new-in",
-//         element: <NewIn />,
-//         loader: productsData,
-//       },
-//     ]
-//   },
-// ]);
-
-// function App() {
-//   return (
-//     <PayPalScriptProvider options={initialOptions}>
-//       <div className='font-bodyFont flex flex-col min-h-screen'>
-//         <RouterProvider router={router}/>
-//       </div>
-//     </PayPalScriptProvider>
-//   );
-// }
-
-// export default App;
-
 import './index.css'
 import Home from './pages/Home'
 import AnnouncementBar from './components/static/AnnouncementBar';
@@ -182,7 +83,7 @@ const router = createBrowserRouter([
     path: "/checkout",
     element: <Checkout />,
   },
-]); // Add a closing bracket here
+]);
 
 function App() {
   return (
--- client/src/components/static/Footer.jsx
+++ client/src/components/static/Footer.jsx
@@ -5,19 +5,31 @@ import SocialLink from "./SocialLink";
 import { CiLocationOn, CiMobile3 } from "react-icons/ci";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsTelephone } from "react-icons/bs";
+import { toast, ToastContainer } from "react-toastify";
+import { useState } from "react";
 
 const Footer = () => {
   const year = new Date().getFullYear();
   const brand = "FILAR";
+  const [email, setEmail] = useState("");
+
+  const handleOnChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubscribe = (e) => {
+    e.preventDefault();
+    toast.success("Thank you for subscribing!");
+    setEmail("");
+  };
 
   return (
     <footer
       className="bg-stone-600 text-center text-white dark:bg-stone-600 dark:text-white lg:text-left">
-      {/* <!-- Main container div: holds the entire content of the footer, including four sections (TW Elements, Products, Useful links, and Contact), with responsive styling and appropriate padding/margins. --> */}
       <div className="mx-6 py-10 text-center md:text-left">
         <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
-          {/* <!-- TW Elements section --> */}
           <div className="">
+          {/* <!-- About us section --> */}
             <h6
               className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
               Filar
@@ -56,9 +68,9 @@ const Footer = () => {
               <CiLocationOn className="mr-3 h-5 w-5" />
               Gabriel Pereira 1330, MVD, UY
             </p>
-            <p className="mb-4 flex items-center justify-center md:justify-start text-sm">
+            <p className="mb-4 flex items-center justify-center md:justify-start text-sm gap-1">
               <MdOutlineEmail className="mr-3 h-5 w-5" />
-              info@filar.com
+              <a href="mailto:info@filar.com">info@filar.com</a>
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start text-sm">
               <CiMobile3 className="mr-3 h-5 w-5" />
@@ -79,10 +91,10 @@ const Footer = () => {
               Find out our latest news!
             </p>
             <p className="mb-4 text-sm">
-              <input className="bg-stone-600 border border-white rounded-md px-2 py-1" type="text" placeholder="Your e-mail" />
+              <input  placeholder="Your e-mail" name='email' value={email} onChange={handleOnChange} className="bg-stone-600 border border-white rounded-md px-2 py-1" type="text"/>
             </p>
             <p>
-              <button className="bg-stone-600 border border-stone-600 rounded-md px-2 py-1 hover:bg-white hover:text-stone-600 text-sm">
+              <button onClick={handleSubscribe} className="bg-stone-600 border border-stone-600 rounded-md px-2 py-1 hover:bg-white hover:text-stone-600 text-sm">
                 Send
               </button>
             </p>
@@ -97,6 +109,18 @@ const Footer = () => {
           href="https://tw-elements.com/"
         > {brand}</a>
       </div>
+      <ToastContainer
+                position="top-left"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
     </footer>
   );
 }
